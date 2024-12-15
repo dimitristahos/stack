1a3c74cd73b0700478ad42f13346a0efaa470931
Dimitris Tahos <dimitristahos@gmail.com>
2024-12-15 11:25:51 +0200

    Create cyberpanel.sh

---
+++ cyberpanel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+### linode
+### Install cyberpanel
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Setup/cybersetup.sh )
+### Regenerate password for Web Admin, Database, setup Welcome Message
+bash <( curl -sk https://raw.githubusercontent.com/litespeedtech/ls-cloud-image/master/Cloud-init/per-instance.sh )
